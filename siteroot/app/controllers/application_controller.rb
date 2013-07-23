b8a21e14781afbca3ed507e3bf547ff781cd44d3
Dan Thompson <dant1@stanford.edu>
2013-07-23 16:27:46 -0600

    ok made a bunch of merges by adding files. now changing files line by line

--- siteroot/app/controllers/application_controller.rb
+++ siteroot/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+  def enki_config
+    @@enki_config = Htcv2::Config.default
+  end
+  helper_method :enki_config
 end
