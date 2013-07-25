830601b24cd0e7736d2a8036e2af60904f275278
Dan Thompson <dant1@stanford.edu>
2013-07-25 16:59:47 -0600

    what have i changed?

--- siteroot/app/controllers/application_controller.rb
+++ siteroot/app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+	def current_ability
+	  @current_ability ||= Ability.new(current_administrator)
+	end
+
   protected
 
   def htcv2_config
