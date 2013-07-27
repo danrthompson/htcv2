fb90bc472a93faf1d3685b8386459a75fa166661
Dan Thompson <dant1@stanford.edu>
2013-07-27 10:13:00 -0600

    blog successfully moved to subdomain

--- siteroot/config/application.rb
+++ siteroot/config/application.rb
@@ -11,6 +11,10 @@ end
 
 module Htcv2
   class Application < Rails::Application
+
+    # for enki
+    config.blog_suburl = 'blog'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
