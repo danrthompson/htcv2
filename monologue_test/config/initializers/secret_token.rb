69bbbc024e2b2a0fc91b53a900ced906c49ad9f4
Dan Thompson <dant1@stanford.edu>
2013-07-27 11:04:51 -0600

    moving on to store - removed alternative blog options and started looking at 2 store options

--- monologue_test/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-MonologueTest::Application.config.secret_token = 'aabbd824e40bed7b05cc92ced736f8bdc3e458a4f9edeb959f866c32b3dcd23660aed0c5157cb9cf6fc6fa9a18962ae3649ecd7e77e29075a2a963f233e497b5'
