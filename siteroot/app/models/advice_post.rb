22a59eeb6506ba34c0439d59c69bf25480ecde50
Dan Thompson <dant1@stanford.edu>
2013-08-01 12:49:23 -0600

    dependencies work. i spent way too much time on that tho because i still am not using proper testing methodology... ill set that up before i do anything else thats test intensive. next step: comments on comments

--- siteroot/app/models/advice_post.rb
+++ siteroot/app/models/advice_post.rb
@@ -3,7 +3,8 @@ class AdvicePost < ActiveRecord::Base
   validates :post_text, :user_id, presence: true
 
   acts_as_commentable
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
+  belongs_to :user
 
   def title
   	post_text.truncate(80, separator: ' ')
--- siteroot/db/migrate/20130801000649_add_user_ids_to_user_creatable_models.rb
+++ siteroot/db/migrate/20130801000649_add_user_ids_to_user_creatable_models.rb
@@ -1,6 +1,6 @@
 class AddUserIdsToUserCreatableModels < ActiveRecord::Migration
   def change
-  	add_column :advice_posts, :user_id, :integer, :null => false
+  	add_column :advice_posts, :user_id, :integer, default: 0, null: false
   	add_index :advice_posts, :user_id
   end
 end
