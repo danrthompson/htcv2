5717f2f2b4b92e555913a05a03fb27d99d36b4e1
Daniel Haarburger <work.haarburger@gmail.com>
2013-08-05 13:20:30 -0600

    implements advice section front end

--- siteroot/db/schema.rb
+++ siteroot/db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(:version => 20130804032959) do
 
   create_table "advice_posts", :force => true do |t|
     t.text     "body"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-    t.integer  "user_id",            :null => false
+    t.datetime "created_at",                        :null => false
+    t.datetime "updated_at",                        :null => false
+    t.integer  "user_id",            :default => 0, :null => false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
@@ -832,7 +832,6 @@ ActiveRecord::Schema.define(:version => 20130804032959) do
     t.datetime "reset_password_sent_at"
     t.datetime "created_at",                                           :null => false
     t.datetime "updated_at",                                           :null => false
-    t.string   "spree_api_key",          :limit => 48
     t.datetime "remember_created_at"
   end
 
