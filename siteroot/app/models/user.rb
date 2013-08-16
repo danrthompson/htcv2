a1ddc20020f36c648ea260cd9cacfe68d4a12157
Dan Thompson <dant1@stanford.edu>
2013-08-15 23:40:10 -0600

    added usernames

--- siteroot/app/models/user.rb
+++ siteroot/app/models/user.rb
@@ -4,8 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :token_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :timeoutable, :lockable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :administrator
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :administrator, :username
   # attr_accessible :title, :body
+
+  validates :username, presence: true, uniqueness: true
 
   has_many :advice_posts
   has_many :comments
--- siteroot/db/schema.rb
+++ siteroot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130807211824) do
+ActiveRecord::Schema.define(:version => 20130816052422) do
 
   create_table "comments", :force => true do |t|
     t.integer  "commentable_id",   :default => 0
@@ -934,6 +934,7 @@ ActiveRecord::Schema.define(:version => 20130807211824) do
     t.boolean  "administrator"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username",               :default => "", :null => false
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
