a74816c421530b9ec9d3d877432071941ee9daee
Dan Thompson <dant1@stanford.edu>
2013-08-15 18:31:56 -0600

    knocked out a bunch of little things. removed read more from post show page, locked out spree and removed it from header, got rid of more posts and archive section for now, set up processing image. next: importing resources and resource categories from gregs files

--- siteroot/app/models/service.rb
+++ siteroot/app/models/service.rb
@@ -3,7 +3,7 @@ class Service < ActiveRecord::Base
 
   has_attached_file :logo, path: '/:class/:attachment/:id_partition/:style/:basename.:extension', styles: {thumb: '250x60'}
 
-  process_in_background :logo, processing_image_url: "https://s3.amazonaws.com/HarnessTheCrowd/HTC-LOGO-SIDE-TRANSPARENT.png"
+  process_in_background :logo, processing_image_url: "https://s3.amazonaws.com/HarnessTheCrowd/admin_uploads/processing-img.png"
 
 
   belongs_to :service_category
