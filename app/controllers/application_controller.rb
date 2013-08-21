3415e4158ff2accbc7d17dd972e88beb6ac11ae6
Dan Thompson <dant1@stanford.edu>
2013-08-20 18:46:24 -0600

    put in proper sign in and sign out buttons and created proper redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def after_sign_in_path_for(resource)
+	'/conversation'
+  end
+
   protected
 
 	def admin_auth_check!
