51240ba743142ef43522df2fef83421122a3fbb7
Daniel Haarburger <work.haarburger@gmail.com>
2013-08-21 23:15:56 -0600

    implements newsletter front end

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Htcv2::Application.routes.draw do
   post 'tools/create_video_request'
   post 'tools/create_tool_suggestion'
   get 'service/:id/:featured/:location' => 'tools#this_service'
+  get '/community' => 'new_features#community_page'
 
   get 'about_us' => 'static#about_us'
   get 'contact_us' => 'static#contact_us'
