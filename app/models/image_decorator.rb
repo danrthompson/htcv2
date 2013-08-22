c0d9cd40e84e37390ad6aa68812537bbec359b20
Dan Thompson <dant1@stanford.edu>
2013-08-21 18:01:00 -0600

    continuing to pare back, seems to still be working

--- app/models/image_decorator.rb
+++
@@ -1,4 +0,0 @@
-Spree::Image.class_eval do
-  attachment_definitions[:attachment][:path] = "/#{ENV['S3_PATH_PREFIX']}store/:id-:hash/:style/:basename.:extension"
-  attachment_definitions[:attachment][:hash_secret] = ENV['PAPERCLIP_HASH_SECRET']
-end
