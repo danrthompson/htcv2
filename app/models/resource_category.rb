cf4787ce8ff77a4246ea1227b3be881f07f6ad97
Dan Thompson <dant1@stanford.edu>
2013-08-26 10:27:37 -0600

    ok wrapping up that index stuff then going back to validations

--- app/models/resource_category.rb
+++ app/models/resource_category.rb
@@ -1,7 +1,10 @@
 class ResourceCategory < ActiveRecord::Base
   attr_accessible :title, :rank, :image, :resource_category_id, :seo_url
 
-  validates :seo_url, presence: true, uniqueness: true
+  before_validation :change_blank_to_nil
+
+	validates :seo_url, uniqueness: true, allow_nil: true
+
   validates :title, :rank, :image, presence: true
 
   has_attached_file :image, path: "/#{ENV['S3_PATH_PREFIX']}:class/:attachment/:id_partition/:style/:basename.:extension"
@@ -9,4 +12,10 @@ class ResourceCategory < ActiveRecord::Base
   has_many :resource_categories
   has_many :resources
   belongs_to :resource_category
+
+  protected
+
+  def change_blank_to_nil
+
+  end
 end
