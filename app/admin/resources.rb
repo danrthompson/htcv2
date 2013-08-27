3d7d42db3235bddfbc0a5a3ed49351fb1f8e523a
Dan Thompson <dant1@stanford.edu>
2013-08-27 14:42:30 -0600

    updated logos

--- app/admin/resources.rb
+++ app/admin/resources.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register Resource do
-  
+
+	index as: :blog
+
 end
+
+
+	# show do |resource|
+
+      # attributes_table do
+      #   row :title
+      #   row :image do
+      #     image_tag(ad.image.url)
+      #   end
+      # end
+	# end
+      
