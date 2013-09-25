e1d200b32790f6de198e628e667bab78bbdea1a0
Dan Thompson <dant1@stanford.edu>
2013-09-24 19:59:20 -0700

    added ebook page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Htcv2::Application.routes.draw do
   get "/422", :to => "static#error_page"
   get "/500", :to => "static#error_page"
 
+  get "/ebook_page", to: "static#ebook_page"
+
 
   root to: 'static#homepage'
   ActiveAdmin.routes(self)
